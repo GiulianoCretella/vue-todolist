0837ab067e28a089a0a4518a62f46e28336aaad9
GiulianoCretella <giulianocretella.1@gmail.com>
2022-04-22 17:40:50 +0200

    bonus 1 completo

--- js/script.js
+++ js/script.js
@@ -63,6 +63,7 @@ const comanda = new Vue({
                 cocktail:this.nuovoCocktail
             }
             this.comandaTav.push(cocktailN);
+            this.nuovoCocktail='';
         }
     }
 })
